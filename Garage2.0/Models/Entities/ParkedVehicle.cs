45416cb6ee24a5b1dab9205b64ab21c516d9b09a
pplnews <rn_mk@hotmail.com>
2021-08-25 11:17:43 +0200

    added text formating

--- Garage2.0/Models/Entities/ParkedVehicle.cs
+++ Garage2.0/Models/Entities/ParkedVehicle.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 
@@ -11,42 +10,42 @@ namespace Garage2._0.Models.Entities
         public ParkedVehicle()
         {
 
-        } 
+        }
         public int Id { get; set; }
 
         [Required]
         [Display(Name = "Vehicle type")]
         public VehicleType VehicleType { get; set; }
 
-        [RegularExpression(@"([a-zA-ZäöåÄÖÅ)]{3})([0-9]{3})$", ErrorMessage ="Invalid format ex: ABC123.")]
+        [RegularExpression(@"([a-zA-ZäöåÄÖÅ)]{3})([0-9]{3})$", ErrorMessage = "Invalid format ex: ABC123.")]
         [Required]
         [MaxLength(6)]
-       // [Remote(action: "VerifyRegNo", controller: "ParkedVehicles")] släckt så länge
+        // [Remote(action: "VerifyRegNo", controller: "ParkedVehicles")] släckt så länge
         [Display(Name = "Registration No")]
         public string RegNo { get; set; }
 
         [RegularExpression(@"[a-zA-ZäöåÄÖÅ)]{3,}$", ErrorMessage = "This is not a valid color")]
-        [MaxLength (10)]
+        [MaxLength(10)]
         public string Color { get; set; }
 
         [Required]
-        [MaxLength(10)]
-        [RegularExpression(@"[a-zA-Z)]{3,}$", ErrorMessage = "This is not a valid Make")]
+        [MaxLength(20)]
+        [RegularExpression(@"[a-zA-Z \s)]{3,}$", ErrorMessage = "This is not a valid Make")]
         public string Make { get; set; }
 
-        [MaxLength(10)]
-        [RegularExpression(@"[a-zA-Z0-9)]{3,}$", ErrorMessage = "This is not a valid Model")]
+        [MaxLength(20)]
+        [RegularExpression(@"[a-zA-Z0-9) \s]{2,}$", ErrorMessage = "This is not a valid Model")]
         public string Model { get; set; }
 
-        [Range(1,12, ErrorMessage = "Number of wheels must be between 1 and 12")]
+        [Range(0, 12, ErrorMessage = "Number of wheels must be between 0 and 12")]
         [Display(Name = "Number of wheels")]
         public int NoOfWheels { get; set; }
 
         [Display(Name = "Arrival time")]
         [DisplayFormat(DataFormatString = "{0:yyy-MM-dd hh:mm:ss}")]
         [DataType(DataType.DateTime)]
-        public DateTime ArrivalTime  { get; set; }
+        public DateTime ArrivalTime { get; set; }
 
     }
-       
+
 }
