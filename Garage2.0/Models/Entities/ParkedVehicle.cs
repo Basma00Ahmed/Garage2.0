f2bda5d7ceb9453330bac6e497be551b8c24f81d
pplnews <rn_mk@hotmail.com>
2021-08-29 09:20:23 +0200

    update color

--- Garage2.0/Models/Entities/ParkedVehicle.cs
+++ Garage2.0/Models/Entities/ParkedVehicle.cs
@@ -24,7 +24,7 @@ namespace Garage2._0.Models.Entities
         [Display(Name = "Registration No")]
         public string RegNo { get; set; }
 
-        [RegularExpression(@"[a-zA-ZäöåÄÖÅ)]{3,}$", ErrorMessage = "This is not a valid color")]
+        [RegularExpression(@"[a-zA-ZäöåÄÖÅ)]{3,}$", ErrorMessage = "This is not a valid Color")]
         [MaxLength(10)]
         public string Color { get; set; }
 
